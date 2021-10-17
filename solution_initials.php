81daa5f30c53ebbb198a24cd1c84149cd63702c2
kud-aa <akudaybergenov@miem.hse.ru>
2021-10-17 18:43:21 +0300

    edited solution initals

--- solution_initials.php
+++ solution_initials.php
@@ -4,10 +4,40 @@ declare(strict_types = 1);
 
 function getInitials(string $FIO)
 {
+    for ($i = 0; $i < mb_strlen($FIO, 'UTF-8'); $i++) {
+        $array[] = mb_substr($FIO, $i, 1, 'UTF-8');
+    }
 
-    return $FIO;
+    for ($i = 0; $i < count($array); $i++){
+        if ($array[$i] == " "){
+            \array_splice($array, 0, $i);
+            break;
+        }
+
+        echo $array[$i];
+    }
+
+    echo " ";
+
+    $n = 0;
+    for ($i = 0; $i < count($array); $i++){
+        if ($array[$i] == " "){ 
+            $n = $i;
+            echo $array[$n+1];
+            echo ".";
+        }
+        if ($array[$i] == "-"){
+            $n = $i;
+            echo $array[$i];
+            echo $array[$n+1];
+            echo ".";
+        }
+
+    }
+
 }
 
-echo
-
+getInitials("мамин-сибиряк дмитрий наркисович");
+echo "\n";
+getInitials("Смирнов Теодор-Арсений");
 ?>
