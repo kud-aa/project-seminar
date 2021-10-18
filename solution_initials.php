1b931207ba7fe5272474caa0fe8941613818b92c
kud-aa <akudaybergenov@miem.hse.ru>
2021-10-18 19:05:29 +0300

    added only name check

--- solution_initials.php
+++ solution_initials.php
@@ -10,6 +10,7 @@ function getInitials(string $FIO)
     // check if FIO is empty
     if (empty($FIO)){
         return null;
+        exit();
     }
 
     $FIO =  mb_convert_case($FIO, MB_CASE_TITLE, "UTF-8");  // convert to utf8
@@ -21,12 +22,19 @@ function getInitials(string $FIO)
     }
 
     // add to output array all chars until meets first space 
+    $n_space = 0;
     for ($i = 0; $i < count($array); $i++){
         if ($array[$i] == " "){
+            $n_space++;
             \array_splice($array, 0, $i);
             break;
         }
         array_push($output, $array[$i]);
+    }
+
+    if($n_space == 0){
+        return null;
+        exit();
     }
 
     array_push($output, " ");                               // space between last name and first name
@@ -52,4 +60,15 @@ function getInitials(string $FIO)
 
 }
 
+//echo getInitials("IvanDrago");
+//echo "\n";
+//echo getInitials("   мамин-сибиряк дмитрий наркисович");
+//echo "\n";
+//echo getInitials("Петров иван");
+//echo "\n";
+//echo getInitials("Маркес Габриэль Хосе Гарсиа");
+//echo "\n";
+//echo getInitials("Смирнов Теодор-Арсений");
+//echo "\n";
+//echo getInitials("Смирнов Теодор-Арсений Ларионов-Тришкин");
 ?>
