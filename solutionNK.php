bad385b1bcfee0e03b38d7e4d5532c8524590d85
kud-aa <akudaybergenov@miem.hse.ru>
2021-11-06 02:33:12 +0300

    updated solutionNK

--- solutionNK.php
+++ solutionNK.php
@@ -1,5 +1,13 @@
 <?php
 
-    phpinfo();
+function getSumNK($input, $N, $K)
+{
+    for($K = 1; $K < $N + $K - 1; $K++)
+    {
+       $sum = $input[$K] + $sum; 
+    }
+}
+
+echo getSumNK([1,3,4,5,8,9], 3, 2);
 
 ?>
