dced497b0f21cce9ea87a588c1054ff78b586b81
kud-aa <akudaybergenov@miem.hse.ru>
2021-11-06 02:48:43 +0300

    draft solutionNK

--- solutionNK.php
+++ solutionNK.php
@@ -2,12 +2,18 @@
 
 function getSumNK($input, $N, $K)
 {
-    for($K = 1; $K < $N + $K - 1; $K++)
+    $last_index = $N + $K - 1;
+    $sum = 0;
+    $i = $K - 1;
+    for($i; $i < $last_index; $i++)
     {
-       $sum = $input[$K] + $sum; 
+       $sum = $input[$i] + $sum; 
     }
+    return $sum;
 }
 
 echo getSumNK([1,3,4,5,8,9], 3, 2);
-
-?>
+echo "\n";
+echo getSumNK([1,3,4], 1, 3);
+echo "\n";
+echo getSumNK([1,2,3,4,5,6,7], 4, 3);
