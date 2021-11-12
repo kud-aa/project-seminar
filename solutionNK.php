786921a7efe0049c63f30d233946c78e562eb3d7
kud-aa <akudaybergenov@miem.hse.ru>
2021-11-12 22:07:34 +0300

    updated solution for keys

--- solutionNK.php
+++ solutionNK.php
@@ -39,39 +39,39 @@ function getSumNK($input, int $N, int $K): ?int
     return array_sum(array_slice($input, $i, $N));
 }
 
-echo getSumNK([1,3,4,5,8,9], 3, 2);
-echo "\n";
-echo getSumNK([1,3,4], 1, 3);
-echo "\n";
-echo getSumNK([1,2,3,4,5,6,7], 4, 3);
-echo "\n";
-echo getSumNK([1,2,3.0], 3, 1);
-echo "\n";
-echo getSumNK([1,2,3], 3, 3);
-echo "\n";
-echo getSumNK([1,2,3], -1, -12);
-echo "\n";
-echo getSumNK([], 1, 2);
-echo "\n";
-echo getSumNK([1,2,3,4,5,6], 4, 0);
-echo "\n";
-echo getSumNK([1,2,3,4,5,6], 0, 2);
-echo "\n";
-echo getSumNK([1,2,3,4,5,6], 0, 0);
-echo "\n";
-echo getSumNK([1,2,3,4,1.0,2], 3, 1);
-
-echo "\n";
-echo "Tests: \n";
-$input = [1,3,4,5,8,9];
-echo getSumNK($input, 3, 2).PHP_EOL; //12
-echo getSumNK($input, 0, 3).PHP_EOL; //30
-echo getSumNK($input, 1, 0).PHP_EOL; //1
-echo getSumNK($input, 1, 1).PHP_EOL; //1
-echo getSumNK($input, 6, 2).PHP_EOL; //-1
-echo getSumNK($input, -2, 2).PHP_EOL; //-1
-echo getSumNK($input, 3, -1).PHP_EOL; //-1
-$input = [1,3,4];
-echo getSumNK($input, 1, 3).PHP_EOL; //4
-$input = [-5=>1, 'cat'=>3, 2.0=>1];
-echo getSumNK($input, 0, 0).PHP_EOL; //5
+#echo getSumNK([1,3,4,5,8,9], 3, 2);
+#echo "\n";
+#echo getSumNK([1,3,4], 1, 3);
+#echo "\n";
+#echo getSumNK([1,2,3,4,5,6,7], 4, 3);
+#echo "\n";
+#echo getSumNK([1,2,3.0], 3, 1);
+#echo "\n";
+#echo getSumNK([1,2,3], 3, 3);
+#echo "\n";
+#echo getSumNK([1,2,3], -1, -12);
+#echo "\n";
+#echo getSumNK([], 1, 2);
+#echo "\n";
+#echo getSumNK([1,2,3,4,5,6], 4, 0);
+#echo "\n";
+#echo getSumNK([1,2,3,4,5,6], 0, 2);
+#echo "\n";
+#echo getSumNK([1,2,3,4,5,6], 0, 0);
+#echo "\n";
+#echo getSumNK([1,2,3,4,1.0,2], 3, 1);
+#
+#echo "\n";
+#echo "Tests: \n";
+#$input = [1,3,4,5,8,9];
+#echo getSumNK($input, 3, 2).PHP_EOL; //12
+#echo getSumNK($input, 0, 3).PHP_EOL; //30
+#echo getSumNK($input, 1, 0).PHP_EOL; //1
+#echo getSumNK($input, 1, 1).PHP_EOL; //1
+#echo getSumNK($input, 6, 2).PHP_EOL; //-1
+#echo getSumNK($input, -2, 2).PHP_EOL; //-1
+#echo getSumNK($input, 3, -1).PHP_EOL; //-1
+#$input = [1,3,4];
+#echo getSumNK($input, 1, 3).PHP_EOL; //4
+#$input = [-5=>1, 'cat'=>3, 2.0=>1];
+#echo getSumNK($input, 0, 0).PHP_EOL; //5
