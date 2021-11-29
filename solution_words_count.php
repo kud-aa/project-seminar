d829cd5946a72419aff1269d4f716668c4b3d426
kud-aa <akudaybergenov@miem.hse.ru>
2021-11-29 19:38:40 +0300

    changed punctuation filter

--- solution_words_count.php
+++ solution_words_count.php
@@ -5,12 +5,16 @@ declare(strict_types = 1);
 function wordsCount (string $sourceString) : ?array
 {
 
+    // First convert string to UTF8
     $sourceString =  mb_strtolower(mb_convert_case($sourceString, MB_CASE_TITLE, "UTF-8"));
 
-    $sourceString = str_replace(array('-', ',', '.' , ';', ':', '"', '`'), '', $sourceString);
+    // Remove all the punctuation marks
+    $sourceString = preg_replace('/[[:punct:]]/', '', $sourceString);
 
+    // Replace all the whitespaces (tabs, new lines, multiple spaces) by single space
     $sourceString = preg_replace('/[[:space:]]/', ' ', $sourceString);
 
+    // The words are now separated by single spaces and can be splitted to an array
     $words_array = array_count_values(preg_split( "/[\n\r\t ]+/", $sourceString, 0, PREG_SPLIT_NO_EMPTY ));
 
     return $words_array;
