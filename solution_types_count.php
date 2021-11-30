c4f8fb937ad74be9a686cf5cf9080f3860649e5e
kud-aa <akudaybergenov@miem.hse.ru>
2021-11-30 21:47:54 +0300

    updated solution

--- solution_types_count.php
+++ solution_types_count.php
@@ -1,7 +1,9 @@
 <?php
 
-function typesCounter (): ?array
+function typesCounter (... $numbers) 
 {
+    return "params: ".var_export($numbers, true).PHP_EOL;
 }
 
+echo typesCounter(1, 'text', [1,2,3], true, new StdClass());
 ?>
