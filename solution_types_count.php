8a97062ffc64c4a795965d045f1baf033d7439a3
kud-aa <akudaybergenov@miem.hse.ru>
2021-12-02 17:29:45 +0300

    one idea solution

--- solution_types_count.php
+++ solution_types_count.php
@@ -1,9 +1,38 @@
 <?php
 
-function typesCounter (... $numbers) 
+function gettype_str ($value): string
 {
-    return "params: ".var_export($numbers, true).PHP_EOL;
+    if (gettype($value) == 'double') return 'float';
+    return gettype($value);
 }
 
-echo typesCounter(1, 'text', [1,2,3], true, new StdClass());
+function typesCounter (... $arguments) 
+{ 
+    $array = array('boolean', 'integer', 'float', 'string', 'object', 'array');
+
+    $nums = array();
+    foreach ($array as $type)
+    {
+        $nums[$type] = 0;
+    }
+
+    foreach($arguments as $arg)
+    {
+        $type= gettype_str ($arg);
+        if (array_key_exists($type, $nums))
+            $nums[$type] += 1;
+        else
+            return null;
+    }
+    return $nums;
+}
+
+print_r(typesCounter('test', new StdClass, false));
+echo "\n";
+print_r(typesCounter(1, 3, 'test', 7, 'another string', 7.16, 1.2e3, 'hoho', 10, true));
+echo "\n";
+print_r(typesCounter("s", true, true));
+echo "\n";
+print_r(typesCounter('test', 1111, null));
+echo "\n";
 ?>
